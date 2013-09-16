e0f26b4af4ce68a6667b001ddd54aaa812bef5c3
Daniel Santiago <daniel.santiago5@upr.edu>
2013-09-16 16:50:12 -0400

    Added small java compile and run prototype using nodejs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var config = require("./lib/config.js"),
     app = express(),
     dbClient = require("mongodb");
 
+var exec = require('child_process').exec;
+
 app.configure(function() {
   app.set("name", config.appName);
 });
@@ -14,6 +16,36 @@ app.use(function (req, res, next) {
 });
 
 app.use(express.static(__dirname + "/public"));
+
+app.use(express.bodyParser({'uploadDir': __dirname + '/tmp'}));
+
+app.post("/jsubmit", function (req, res) {
+  console.log(req.files.jfile);
+  var fileName = req.files.jfile.originalFilename;
+  var className = fileName.substring(0, fileName.length-5);
+  var filePath = req.files.jfile.path;
+  var cmd = 'javac ' + '-d "' + __dirname + '/tmp" ' + filePath;
+  console.log(cmd);
+  var javac = exec(cmd, function (err, stdout, stderr) {
+    if(err) {
+      res.send(stderr);
+    } else {
+      var runJava = exec('cd ' + __dirname + '/tmp; java ' + className,
+        function (err, stdout, stderr) {
+          if(err) {
+            res.send(stderr);
+          } else {
+            res.send(stdout);
+          }
+        });
+    }
+  });
+});
+
+app.use(function (err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
 
 dbClient.connect(config.dbAddress, function (err, db) {
   if(err) {
