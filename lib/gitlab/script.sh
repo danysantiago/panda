3990e4f4d83da17d4112f0763ac35c1c058823ad
Nelian Colon <nelian.colon@upr.edu>
2013-10-14 19:52:56 -0400

    uploaded node-gitlab modules and some gitlab functions

---
+++ lib/gitlab/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir $1
+cd $1
+git init
+touch README
+git add README
+git commit -m 'first commit'
+git remote add origin http://$2:$3@pandagitlab.sytes.net/$2/$1.git
+git push -u origin master
+cd ..
+rm -rf $1
