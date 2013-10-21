21c12f7d1ca8161ff97ec5b688b51a793c67ea1b
Nelian Colon <nelian.colon@upr.edu>
2013-10-21 17:37:26 -0400

    commented models and script, made script better and consolidate them in one, changed create project example

---
+++ lib/gitlab/createRepo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# usage: ./createRepo.sh 1 2 3 4 5
+# 1: project name (required)
+# 2: username (required)
+# 3: password (required)
+# 4: project owner username (required)
+# 5: archive file (project skeleton) (optional)
+
+mkdir $1 #create temp project directory
+
+cd $1 #move to temp directory
+
+git init #initialize git repository
+
+touch README #create dummy read me file
+
+git add README #add dummy read me to git tracking files
+
+if [ $# -eq 5 ] ; then #if archive file
+	if [ grep -q ".zip" <<< $5 ] ; then #if .zip
+		unzip ../$5 #decompress zip file
+	elif [ grep -q ".tar" <<< $5 ] ; then #if tar file
+		tar -xvzf ../$5 #decompress tar file
+	fi
+fi
+
+git add * #add all new files to git tracking files
+
+git commit -m 'first commit' #do first commit
+
+git remote add origin http://$2:$3@pandagitlab.sytes.net/$4/$1.git #add origin to repository
+
+git push -u origin master #push to master
+
+cd .. #go to original folder
+ 
+rm -rf $1 #remove temp folder
