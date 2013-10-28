ba31d1c8ba03d1584c39ed9d15b9b309e81bcd03
Daniel Santiago <daniel.santiago5@upr.edu>
2013-10-27 21:56:15 -0400

    Some code cleaning, testing jshint

--- patches/patch.sh
+++ patches/patch.sh
@@ -3,4 +3,6 @@
 #Patches to apply after modules installation
 
 #Cover patch to work with Node 0.10.17+ (https://github.com/itay/node-cover/pull/39)
-patch ./node_modules/cover/bin/cover ./patches/cover.patch
+patch -sN ./node_modules/cover/bin/cover ./patches/cover.patch
+
+exit 0
