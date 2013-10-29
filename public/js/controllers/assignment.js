c261ce882d6b0b3783c74b87139119bb224c6984
Samuel <samus250@gmail.com>
2013-10-28 21:39:16 -0400

    Added capability to change tabs in assignment view.

--- public/js/controllers/assignment.js
+++ public/js/controllers/assignment.js
@@ -1,5 +1,39 @@
 pandaApp.controller('AssignmentController', ['$scope', 'currentUser', 'assignment',
     function($scope, currentUser, assignment) {
   $scope.assignment = assignment;
+
+  $scope.isInfoVisible = true;
+  $scope.isSubmissionsVisible = false;
+  $scope.isTestCasesVisible = false;
+  $scope.isRepositoryVisible = false;
+
+  $scope.showInfo = function() {
+    $scope.isInfoVisible = true;
+    $scope.isSubmissionsVisible = false;
+    $scope.isTestCasesVisible = false;
+    $scope.isRepositoryVisible = false;
+  };
+
+  $scope.showSubmissions = function() {
+    $scope.isInfoVisible = false;
+    $scope.isSubmissionsVisible = true;
+    $scope.isTestCasesVisible = false;
+    $scope.isRepositoryVisible = false;
+  };
+
+  $scope.showTestCases = function() {
+    $scope.isInfoVisible = false;
+    $scope.isSubmissionsVisible = false;
+    $scope.isTestCasesVisible = true;
+    $scope.isRepositoryVisible = false;
+  };
+
+  $scope.showRepository = function() {
+    $scope.isInfoVisible = false;
+    $scope.isSubmissionsVisible = false;
+    $scope.isTestCasesVisible = false;
+    $scope.isRepositoryVisible = true;
+  };
+
 }]);
 
