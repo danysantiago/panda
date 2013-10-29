4c4d543d26e51e47b82fb6e36da6549f167ca4a7
Samuel <samus250@gmail.com>
2013-10-29 17:31:26 -0400

    Fixed the account page.

--- public/js/controllers/account.js
+++ public/js/controllers/account.js
@@ -3,4 +3,17 @@
  */
 pandaApp.controller('AccountController', ['$scope', 'currentUser',
     function($scope, $currentUser) {
+  $scope.isProfileVisible = true;
+  $scope.isSecurityVisible = false;
+
+  $scope.showProfile = function() {
+    $scope.isProfileVisible = true;
+    $scope.isSecurityVisible = false;
+  };
+
+  $scope.showSecurity = function() {
+    $scope.isProfileVisible = false;
+    $scope.isSecurityVisible = true;
+  };
+
 }]);
