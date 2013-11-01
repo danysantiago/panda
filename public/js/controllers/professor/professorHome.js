5d73c0459bc69919525c986631c4ccaa97b7ed95
Samuel <samus250@gmail.com>
2013-10-31 22:25:08 -0400

    Added assignment filtering in professor home.

--- public/js/controllers/professor/professorHome.js
+++ public/js/controllers/professor/professorHome.js
@@ -3,18 +3,74 @@
  * 
  */
 
-pandaApp.controller('ProfessorHomeController', ['$scope', 'currentUser', 'User',
-    function($scope, currentUser, User) {
+/**
+ * Filter with multiple params
+ */
+pandaApp.filter('filterByAttributes', function() {
+  return function(array, query, attributes) {
+    if (!array || !query) {
+      return array;
+    }
+
+    // Returns an array filtered by the params. Params is an array of object
+    // attributes that we wish to filter.
+    return array.filter(function(element) {
+      for (var i = 0; i < attributes.length; i++) {
+        if (element[attributes[i]].toLowerCase().indexOf(
+            query.toLowerCase()) > -1) {
+          return true;
+        };
+      }
+      return false;
+    });
+  };
+});
+
+pandaApp.controller('ProfessorHomeController', ['$scope', 'currentUser', 'User', 'Course',
+    function($scope, currentUser, User, Course) {
 
   var user = User.get({id: currentUser._id, submissions: true,
       assignments: true, courses: true}, function() {
     // TODO(samuel): Extract pending assignments first.
     $scope.user = user;
+    var assignments = [];
+
+    /*
+        __,___@
+       [_'^   )    Oink Oink: This is for getting the assignments of a professor
+         `//-\\    into user.assignments :@
+         ^^  ^^
+    */
+    user.courses.forEach(function(course, index) {
+      var course = Course.get({id: course._id, assignments: true}, function() {
+        // Nested oinky oink: We need to reference each course code with an
+        // assignment some how. This is the dirty way to do it.
+        course.assignments.forEach(function(assignment) {
+          assignment.courseCode = course.code;
+        });
+
+        // Concat == flatten.
+        assignments = assignments.concat(course.assignments);
+        if (index === user.courses.length - 1) {
+          $scope.user.assignments = assignments;
+        }
+      });
+    });
   });
 
   $scope.toggleCourseModal = function() {
     // Call the modal just how you do with bootstrap jqueried
     $('#createCourseModal').modal();
+  };
+
+  $scope.toggleAssignmentModal = function() {
+    $('#createAssignmentModal').modal();
+  };
+
+  $scope.postThat = function(courseData) {
+    //
+   //2 + 3;
+    
   };
 
 }]);
--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
  * The Panda AngularJS app. :)
  */
 var pandaApp = angular.module('pandaApp', ['panda.services',
-  'http-auth-interceptor', 'ngRoute']);
+  'http-auth-interceptor', 'ngRoute', 'ui.bootstrap']);
 
 /**
  * This is the resolve map for the current logged in user that will be injected
