0450b08962d4e5948c4b7f13cc10036c7eeea650
Samuel <samus250@gmail.com>
2013-11-02 14:43:34 -0400

    Close the modal after creating a new assignment, and auto-refresh the page so that the table includes the new assignment.

--- public/js/controllers/professor/professorAssignments.js
+++ public/js/controllers/professor/professorAssignments.js
@@ -4,43 +4,45 @@
 pandaApp.controller('ProfessorAssignmentsController', ['$scope', 'currentUser',
     'User', 'Course', 'Assignment',
         function($scope, currentUser, User, Course, Assignment) {
-  // TODO(samuel): Initialize this better, since the query to the user is
-  // async.
   $scope.user = {};
 
-  var user = User.get({id: currentUser._id, submissions: true,
-      assignments: true, courses: true}, function() {
-    // TODO(samuel): Extract pending assignments first.
-    $scope.user = user;
+  // The reason why we are doing this is because we need to refresh the whole
+  // user each time an assignment is created. This is done so that when you
+  // create an assignment, the assignments table is automagically refreshed.
+  ($scope.refreshUser = function() {
+    var user = User.get({id: currentUser._id, submissions: true,
+        assignments: true, courses: true}, function() {
+      $scope.user = user;
 
-    // Set the default course when creating a new assignment. This is optional.
-    $scope.newAssignment.Course = user.courses[0];
+      // Set the default course when creating a new assignment.
+      $scope.newAssignment.Course = user.courses[0];
 
-    /*
-        __,___@
-       [_'^   )    Oink Oink: This is for getting the assignments of a professor
-         `//-\\    into user.assignments :@
-         ^^  ^^
-    */
-    var assignments = [];
-    async.each(user.courses, function(course, done) {
-      var course = Course.get({id: course._id, assignments: true}, function() {
-        // Nested oinky oink: We need to reference each course with an
-        // assignment some how. This is the way to do it.
-        course.assignments.forEach(function(assignment) {
-          assignment.courseCode = course.code;
-          assignment.course = course;
+      /*
+          __,___@
+         [_'^   )    Oink Oink: This is for getting the assignments of a
+           `//-\\    professor into user.assignments :@
+           ^^  ^^
+      */
+      var assignments = [];
+      async.each(user.courses, function(course, done) {
+        var course = Course.get({id: course._id, assignments: true},
+            function() {
+          // Nested oinky oink: We need to reference each course with an
+          // assignment some how. This is the way to do it.
+          course.assignments.forEach(function(assignment) {
+            assignment.courseCode = course.code;
+            assignment.course = course;
+          });
+
+          // Concat == flatten.
+          assignments = assignments.concat(course.assignments);
+          done();
         });
-
-        // Concat == flatten.
-        assignments = assignments.concat(course.assignments);
-        done();
+      }, function () {
+          $scope.user.assignments = assignments;
       });
-    }, function () {
-        $scope.user.assignments = assignments;
     });
-
-  });
+  })(); // Init the user as well :) wii
 
   $scope.toggleAssignmentModal = function() {
     $('#createAssignmentModal').modal();
@@ -48,7 +50,7 @@ pandaApp.controller('ProfessorAssignmentsController', ['$scope', 'currentUser',
 
   $scope.newAssignment = {
     name: '',
-    Course: null, // this is first an object, but the post will post the id str
+    Course: null, // This is first an object, but the post will post the id str
     shortDescription: '',
     deadline: '',
     numOfTries: 0,
@@ -59,6 +61,10 @@ pandaApp.controller('ProfessorAssignmentsController', ['$scope', 'currentUser',
     assignmentInfo.Course = assignmentInfo.Course._id;
     var newAssignment = new Assignment(assignmentInfo);
     newAssignment.$save();
+    $('#createAssignmentModal').modal('hide');
+
+    // Refresh data so that the table contains the newly added assignment.
+    $scope.refreshUser();
   }
 
   var fieldNames = {'name': false, 'courseCode': false, 'creationDate': false,
@@ -79,5 +85,4 @@ pandaApp.controller('ProfessorAssignmentsController', ['$scope', 'currentUser',
       }
     });
   };
-
 }]);
