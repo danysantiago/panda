737d01554da3c2060a45d3c9f0e9fae81bd717a3
Samuel <samus250@gmail.com>
2013-11-02 15:08:32 -0400

    Fixed some db stuff (bad syntax, and object id was not being referenced correctly). Create course now works in home, and the page is also automatically refreshed :)

--- public/js/controllers/professor/professorHome.js
+++ public/js/controllers/professor/professorHome.js
@@ -26,37 +26,44 @@ pandaApp.filter('filterByAttributes', function() {
   };
 });
 
-pandaApp.controller('ProfessorHomeController', ['$scope', 'currentUser', 'User', 'Course',
-    function($scope, currentUser, User, Course) {
+pandaApp.controller('ProfessorHomeController', ['$scope', 'currentUser', 'User',
+    'Course', 'Assignment',
+    function($scope, currentUser, User, Course, Assignment) {
 
-  var user = User.get({id: currentUser._id, submissions: true,
-      assignments: true, courses: true}, function() {
-    // TODO(samuel): Extract pending assignments first.
-    $scope.user = user;
-    var assignments = [];
+  ($scope.refreshUser = function() {
+    var user = User.get({id: currentUser._id, submissions: true,
+        assignments: true, courses: true}, function() {
+      $scope.user = user;
 
-    /*
-        __,___@
-       [_'^   )    Oink Oink: This is for getting the assignments of a professor
-         `//-\\    into user.assignments :@
-         ^^  ^^
-    */
-    user.courses.forEach(function(course, index) {
-      var course = Course.get({id: course._id, assignments: true}, function() {
-        // Nested oinky oink: We need to reference each course code with an
-        // assignment some how. This is the dirty way to do it.
-        course.assignments.forEach(function(assignment) {
-          assignment.courseCode = course.code;
+      // Set the default course when creating a new assignment.
+      $scope.newAssignment.Course = user.courses[0];
+
+      /*
+          __,___@
+         [_'^   )    Oink Oink: This is for getting the assignments of a
+           `//-\\    professor into user.assignments :@
+           ^^  ^^
+      */
+      var assignments = [];
+      async.each(user.courses, function(course, done) {
+        var course = Course.get({id: course._id, assignments: true},
+            function() {
+          // Nested oinky oink: We need to reference each course with an
+          // assignment some how. This is the way to do it.
+          course.assignments.forEach(function(assignment) {
+            assignment.courseCode = course.code;
+            assignment.course = course;
+          });
+
+          // Concat == flatten.
+          assignments = assignments.concat(course.assignments);
+          done();
         });
-
-        // Concat == flatten.
-        assignments = assignments.concat(course.assignments);
-        if (index === user.courses.length - 1) {
+      }, function () {
           $scope.user.assignments = assignments;
-        }
       });
     });
-  });
+  })(); // Init the user as well :) wii
 
   $scope.toggleCourseModal = function() {
     // Call the modal just how you do with bootstrap jqueried
@@ -67,10 +74,40 @@ pandaApp.controller('ProfessorHomeController', ['$scope', 'currentUser', 'User',
     $('#createAssignmentModal').modal();
   };
 
-  $scope.postThat = function(courseData) {
-    //
-   //2 + 3;
-    
+  $scope.newCourse = {
+    name: '',
+    code: '',
+    year: '',
+    semester: ''
   };
+
+  $scope.createCourse = function(courseInfo) {
+    courseInfo.grader = $scope.user._id;
+    var newCourse = new Course(courseInfo);
+    newCourse.$save();
+    $('#createCourseModal').modal('hide');
+
+    // Refresh data so that the table contains the newly added course.
+    $scope.refreshUser();
+  };
+
+  $scope.newAssignment = {
+    name: '',
+    Course: null, // This is first an object, but the post will post the id str
+    shortDescription: '',
+    deadline: '',
+    numOfTries: 0,
+  };
+
+  $scope.createAssignment = function(assignmentInfo) {
+    // Post attribute Course is a string id.
+    assignmentInfo.Course = assignmentInfo.Course._id;
+    var newAssignment = new Assignment(assignmentInfo);
+    newAssignment.$save();
+    $('#createAssignmentModal').modal('hide');
+
+    // Refresh data so that the table contains the newly added assignment.
+    $scope.refreshUser();
+  }
 
 }]);
