d19461e5010405619b2cadbc0ce4fb96aefb5a5e
Samuel <samus250@gmail.com>
2013-11-04 19:59:40 -0400

    Finishing up student assignment.

--- public/js/controllers/student/studentAssignment.js
+++ public/js/controllers/student/studentAssignment.js
@@ -1,13 +1,60 @@
-pandaApp.controller('AssignmentController', ['$scope', 'currentUser', 'assignment',
-    function($scope, currentUser, assignment) {
+pandaApp.controller('AssignmentController', ['$scope', 'currentUser',
+    'assignment', function($scope, currentUser, assignment) {
   $scope.assignment = assignment;
+  $scope.user = currentUser;
 
+  // Filter the submissions of this assignment. We only need this particular
+  // student's submissions.
+  assignment.submissions = assignment.submissions.filter(function(submission) {
+    return submission.user._id === currentUser._id;
+  });
+
+  // Determine the student's score in the assignment and also each of the
+  // submission's totalTime.
+  var studentScore = 0;
+  assignment.submissions.forEach(function(submission) {
+    studentScore += submission.score;
+
+    var submissionCpuTime = 0.0;
+    submission.tests.forEach(function(test) {
+      submissionCpuTime += parseFloat(test.result['cpu usage']);
+    });
+    submission.cpuTime = submissionCpuTime + ' seconds';
+  });
+
+  // Sort submissions by date.
+  assignment.submissions.sort(function(a, b) {
+    if (a.submitDate > b.submitDate) {
+      return -1;
+    } else if (a.submitDate < b.submitDate) {
+      return 1;
+    } else {
+      // A student shouldn't have two submissions at the same time, but it is
+      // a possibility.
+      return 0;
+    }
+  });
+
+  // Add the last submission date
+  if (assignment.submissions.length === 0) {
+    assignment.lastSubmissionDate = "You have no submissions.";
+  } else {
+    assignment.lastSubmissionDate = assignment.submissions[0].submitDate;
+  }
+
+  $scope.scores = {
+    totalScore: assignment.totalScore,
+    studentScore: studentScore
+  };
+
+  $scope.isDefined = angular.isDefined;
+
+  // Tab stuff.
   $scope.infoTab = true;
   $scope.submissionsTab = false;
-  $scope.testCasesTab = false;
   $scope.repositoryTab = false;
 
-  var tabNames = ['infoTab', 'submissionsTab', 'testCasesTab', 'repositoryTab'];
+  var tabNames = ['infoTab', 'submissionsTab', 'repositoryTab'];
 
   $scope.showTab = function (view) {
     tabNames.forEach(function (tabName) {
@@ -20,4 +67,3 @@ pandaApp.controller('AssignmentController', ['$scope', 'currentUser', 'assignmen
   };
 
 }]);
-
