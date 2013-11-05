52266af0473aa3b7ce332e26ea0e73a6640fd7b2
Samuel <samus250@gmail.com>
2013-11-05 13:29:56 -0400

    Cheap student grades view.

--- public/js/controllers/student/studentGrades.js
+++ public/js/controllers/student/studentGrades.js
@@ -2,13 +2,42 @@
  * This is the js file for the assignments route / controller (assignments.html)
  */
 pandaApp.controller('GradesController', ['$scope', 'currentUser', 'User',
-    function($scope, currentUser, User) {
-  // TODO(samuel): Initialize this better, since the query to the user is
-  // async.
+    'Course', function($scope, currentUser, User, Course) {
   $scope.user = {};
+  $scope.courses = [];
 
-  var user = User.get({id: currentUser._id, submissions: true,
-      assignments: true, courses: true}, function() {
+  // courses/:id?users=true gives users with all the grades, &assignments=true
+  // gives you total score and number of assignments.
+  var user = User.get({id: currentUser._id, courses: true}, function() {
     $scope.user = user;
+
+    // TODO(samuel): async
+    user.courses.forEach(function(course) {
+      var c = Course.get({id: course._id, users: true, assignments: true},
+          function() {
+        // Filter out unneccessary users. After this, c.users.length should be 1
+        c.users.filter(function(user) {
+          return user._id === currentUser._id;
+        });
+
+        // Count student accumulated score.
+        c.users.forEach(function(user) {
+          var accumulatedScore = 0;
+          user.grades.forEach(function(grade) {
+            accumulatedScore += grade.score;
+          });
+          c.userAccumulatedScore = accumulatedScore;
+        });
+
+        // Count total course score.
+        var courseTotalScore = 0;
+        c.assignments.forEach(function(assignment) {
+          courseTotalScore += assignment.totalScore;
+        });
+        c.courseTotalScore = courseTotalScore;
+
+        $scope.courses.push(c);
+      });
+    })
   });
 }]);
