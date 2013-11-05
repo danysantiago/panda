3811e2cfc68b63a3043c66d903a5088e12656d0c
Daniel Santiago <daniel.santiago5@upr.edu>
2013-11-05 16:22:04 -0400

    Documented a little more, for HH

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
+/**
+  App.js - Node.js server start point.
+**/
+
 var config = require('./lib/config/config.js'),
     express = require('express'),
     app = express(),
-    mongodb = require('mongodb');
-    bunyan = require('bunyan');
+    mongodb = require('mongodb'),
+    bunyan = require('bunyan'),
+    path = require('path');
 
 var Database = require('./lib/models/database.js');
 
+// Logger object, log lvl is set by config file.
 var log = bunyan.createLogger({'name': 'panda', 'level': config.debugLvl});
 
 app.configure(function() {
   app.set('name', config.appName);
 
-  var env = app.get("env"); //Get node enviroment
-  if (env === "development") {
-    log.warn("Server Configured for Development Mode");
+  var env = app.get('env'); //Get node environment variable
 
-    // App kill route to finish running after tests complete.
-    app.use("/kill/after/tests", function (req, res, next) {
-      res.send(200, "Bye, Bye");
-      log.warn("Server Kill Requested.");
+  if (env === 'development') { //Development env
+    log.warn('Server Configured for Development Mode');
+
+    // On development env we have kill route for server restarting purposes.
+    app.use('/kill/after/tests', function (req, res, next) {
+      res.send(200, 'Bye, Bye');
+      log.warn('Server Kill Requested.');
       process.exit(1);
     });
 
-  } else if (env === "production") {
-    log.warn("Server configured for production mode.");
+  } else if (env === 'production') { //Production env
+    log.info('Server configured for production mode.');
   }
 
   //Use temp folder cleaner
-  require('./lib/tmpClean.js')(config.root + 'tmp', log);
+  require('./lib/tmpClean.js')(path.join(config.root,'tmp'), log);
 });
 
 // Request logger middleware
@@ -41,11 +48,11 @@ app.use( function (req, res, next) {
 app.use(express.compress({
   filter: function (req, res) {
       return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
-  },
+    },
   level: 9
 }));
 
-// Static files middleware
+// Static files middleware, serves Angular.js app and resources
 app.use(express.favicon(config.root + '/public/favicon.ico'));
 app.use(express.static(config.root + '/public'));
 
@@ -59,7 +66,7 @@ app.use(express.session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Database object middleware
+// Database object middleware, passes db object across routes.
 app.use(function (req, res, next) {
   req.db = Database.getDB();
   next();
@@ -69,10 +76,10 @@ app.use(function (req, res, next) {
 app.use('/auth', require('./lib/routes/auth.js'));
 
 // Api Routes
-api = express();
+var api = express();
 api.use(function (req, res, next) {
   // Exception - creating users without logging in
-  if(req.url === "/users" && req.method === "POST") {
+  if(req.url === '/users' && req.method === 'POST') {
     return next();
   }
 
@@ -98,26 +105,27 @@ app.get('/test/queue', function (req, res, next) {
   });
 });
 
-// Error Handler
+// Error Handler middleware
 app.use(function (err, req, res, next){
   log.error(err.stack);
   res.send(500, '500 - Something broke!');
 });
 
-// Not Found
+// Not Found middleware
 app.use(function (req, res, next){
-  res.send(404, '404 - What your looking for is in "El Gara"');
+  res.send(404, '404 - What your looking for is in \'El Gara\'');
 });
 
 //TODO (Daniel): CXheck Jail has been properly setup (/proc), before
 //continuing...
 
+//Connect to DB and start port listening
 mongodb.connect(config.dbAddress, function (err, db) {
   if(err) {
     return log.error('Could not connect to mongodb', err);
   }
 
-  Database.setDB(db);
+  Database.setDB(db); //Set DB singleton
   log.info('Database connection successful - ' + config.dbAddress);
 
   app.listen(config.appPort);
