d40b477671d5a339abd9451c064d70fe1f27c97f
Samuel <samus250@gmail.com>
2013-11-04 23:18:32 -0400

    Finished student submission page.

--- public/js/controllers/student/studentSubmissions.js
+++ public/js/controllers/student/studentSubmissions.js
@@ -2,13 +2,43 @@
  * This is the js file for the submissions route (submissions.html)
  */
 pandaApp.controller('SubmissionsController', ['$scope', 'currentUser', 'User',
-    function($scope, currentUser, User) {
-  // TODO(samuel): Initialize this better, since the query to the user is
-  // async.
+    'Assignment', function($scope, currentUser, User, Assignment) {
   $scope.user = {};
 
-  var user = User.get({id: currentUser._id, submissions: true,
-      assignments: true, courses: true}, function() {
+  // Iterate through assignments and then through the submissions
+  var user = User.get({id: currentUser._id, assignments: true, courses: true,
+      submissions: true}, function() {
     $scope.user = user;
+
+    user.submissions.forEach(function(submission) {
+      // Insert courses into submissions.
+      user.courses.forEach(function(course) {
+        if (course._id === submission.Course) {
+          submission.course = course;
+        }
+      });
+
+      // Insert assignments into submissions
+      user.assignments.forEach(function(assignment) {
+        if (assignment._id === submission.Assignment) {
+          submission.assignment = assignment;
+        }
+      });
+
+      // Each submission needs its testsPassed and cpuTime calculated.
+      submission.testsPassed = 0;
+      submission.cpuTime = 0.0;
+      submission.tests.forEach(function(test) {
+        if (test.result.verdict.toLowerCase() === 'correct') {
+          submission.testsPassed++;
+        }
+
+        submission.cpuTime += parseFloat(test.result['cpu usage']);
+      });
+
+      // Also, for facilitating filtering, each submission needs shortcuts for:
+      submission.courseName = submission.course.name;
+      submission.assignmentName = submission.assignment.name;
+    });
   });
 }]);
