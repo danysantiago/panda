b6a50dfb473f7c29fc3a16d06122a280fdcaebcb
Nelian Colon <nelian.colon@upr.edu>
2013-11-05 16:02:43 -0400

    uncommented send email lines

--- test/module/sendgrid.js
+++ test/module/sendgrid.js
@@ -1,27 +1,26 @@
 var config = require('../../lib/config/config.js'),
-    expect = require('chai').expect,
-    Sendgrid = require('../../lib/sendgrid/Sendgrid');
+expect = require('chai').expect,
+Sendgrid = require('../../lib/sendgrid/Sendgrid');
 
 var sendgrid = new Sendgrid();
 
 var test = function() {
-
   var params = {
-  	from: 		'no-reply@pandacode.sytes.net',
-		to:       'nelian.colon@upr.edu',
-		subject: 	'Hello World',
-		text: 		'Email sent for happy hour'
-	}
+    from: 		'no-reply@pandacode.sytes.net',
+    to:       'nelian.colon@upr.edu',
+    subject: 	'Hello World',
+    text: 		'Email sent for happy hour'
+  }
 
   describe('Sendgrid', function() {
     //send an email from no-reply@pandacode.sytes.net
-   	// it('Send Email', function(done) {
-	   //  sendgrid.sendEmail(params, function (err, json){
-	   //   expect(err).to.not.exist;
-	   //   expect(json.message).to.equal('success');
-    //    done();
-    //   });
-    // });
+    it('Send Email', function(done) {
+      sendgrid.sendEmail(params, function (err, json){
+        expect(err).to.not.exist;
+        expect(json.message).to.equal('success');
+        done();
+      });
+    });
   });
 };
 
