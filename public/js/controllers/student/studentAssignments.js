d681508e78ba780405886fb939e7fa39517602fe
Samuel <samus250@gmail.com>
2013-11-24 15:22:15 -0400

    Fixed up student pending assignments view.

--- public/js/controllers/student/studentAssignments.js
+++ public/js/controllers/student/studentAssignments.js
@@ -3,57 +3,76 @@
  */
 
 pandaApp.controller('AssignmentsController', ['$scope', 'currentUser', 'User',
-    function($scope, currentUser, User) {
-  // TODO(samuel): Initialize this better, since the query to the user is
-  // async.
+    'Course', function($scope, currentUser, User, Course) {
   $scope.user = {};
 
-  /**
-   * Inserts the corresponding courses into the assignments by cross-referencing
-   * the ids of courses and assignments.
-   */
-  var insertCoursesIntoAssignments = function(courses, assignments) {
-    // Build map of courses.
-    var coursesMap = [];
-    courses.forEach(function(course) {
-      coursesMap[course._id] = course;
+  User.get({id: currentUser._id, assignments: true}, function(user) {
+    async.forEach(user.assignments, function(assignment, done) {
+      Course.get({id: assignment.Course}, function(course) {
+        // put each course in the assignment
+        assignment.course = course;
+        // For sorting the table:
+        assignment.courseName = course.name;
+        assignment.courseCode = course.code;
+        done();
+      }, function() {
+        // could not get course...
+        done();
+      });
+    }, function() {
+      // all done
+      var pendingAssignments = user.assignments.filter(function(assignment) {
+        var date = new Date();
+        var deadline = new Date(assignment.deadline);
+        return date <= deadline;
+      });
+
+      $scope.user = user;
+      $scope.user.pendingAssignments = pendingAssignments;
+
+      if (pendingAssignments.length === 0) {
+        // It's better to show all assignments.
+        $scope.showPendingFunction(false);
+      } else {
+        $scope.showPendingFunction(true);
+      }
+
     });
-
-    // Each assignment has only one course.
-    assignments.forEach(function(assignment) {
-      assignment.course = coursesMap[assignment.Course];
-      assignment.courseName = assignment.course.name;
-      assignment.courseCode = assignment.course.code;
-    });
-  };
-
-  var user = User.get({id: currentUser._id, submissions: true,
-      assignments: true, courses: true}, function() {
-    insertCoursesIntoAssignments(user.courses, user.assignments);
-    $scope.user = user;
-
-    // Filter out the assignments by date.
-    var pendingAssignments = user.assignments.filter(function(assignment) {
-      var date = new Date();
-      var deadline = new Date(assignment.deadline);
-      return date <= deadline;
-    });
-
-    user.pendingAssignments = pendingAssignments;
-
   });
 
-  $scope.showPending = false;
-  ($scope.togglePending = function() {
-    $scope.showPending = !$scope.showPending;
+  ($scope.showPendingFunction = function(b) {
+    $scope.showPending = b;
     if ($scope.showPending) {
       $scope.buttonMessage = "Show All";
       $scope.pageTitle = "Pending Assignments";
-      $scope.assignmentsToShow = user.pendingAssignments;
+      $scope.assignmentsToShow = $scope.user.pendingAssignments;
     } else {
       $scope.buttonMessage = "Show Pending";
       $scope.pageTitle = "Assignments";
-      $scope.assignmentsToShow = user.assignments;
+      $scope.assignmentsToShow = $scope.user.assignments;
     }
-  })();
+  })(true);
+
+  $scope.togglePending = function() {
+    $scope.showPendingFunction(!$scope.showPending);
+  };
+
+  // For ordering the assignments in the course page.
+  var fieldNames = {'name': false, 'courseName': false, 'deadline': false};
+
+  $scope.predicate = 'name';
+  $scope.reverseOrder = fieldNames[$scope.predicate];
+
+  $scope.toggleOrder = function(field) {
+    Object.keys(fieldNames).forEach(function(fieldName) {
+      if (fieldName === field) {
+        $scope.predicate = field;
+        fieldNames[field] = !fieldNames[field];
+        $scope.reverseOrder = fieldNames[field];
+      } else {
+        fieldNames[fieldName] = false;
+      }
+    });
+  };
+
 }]);
