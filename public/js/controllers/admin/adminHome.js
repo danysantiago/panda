585489e590ea9caefc20ee722a1f916cc79c3657
Samuel <samus250@gmail.com>
2013-11-25 00:01:22 -0400

    Added functionality for admin to create prof wii.

--- public/js/controllers/admin/adminHome.js
+++ public/js/controllers/admin/adminHome.js
@@ -152,4 +152,103 @@ pandaApp.controller('AdminHomeController', ['$scope', 'currentUser', 'users',
       }
     });
   };
+
+  $scope.toggleSignUpModal = function() {
+    $('#signup-modal').modal();
+  };
+
+  $scope.hideSignUpSuccessModal = function() {
+    $('#signup-modal-success').modal('hide');
+  };
+
+  $scope.hideSignUpErrorModal = function() {
+    $('#signup-modal-error').modal('hide');
+    $('#signup-modal').modal();
+  };
+
+  $scope.hideIncorrectPasswordModal = function() {
+    $('#incorrect-password-modal').modal('hide');
+  };
+
+  $scope.hideServiceUnavailableModal = function() {
+    $('#service-unavailable-modal').modal('hide');
+  };
+
+  $scope.newUser = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+    confirmPassword: '',
+    errorMessage: [],
+    isProfessor: false
+  };
+
+  $scope.createUser = function() {
+    if ($scope.newUser.password !== $scope.newUser.confirmPassword) {
+      // Try again
+      $('#signup-modal').modal('hide');
+      $('#signup-modal-error').modal();
+      $scope.newUser.errorMessage.push(
+          'Please verify the information you have entered. ');;
+      return;
+    }
+
+    if ($scope.newUser.password.length < 6) {
+      $('#signup-modal').modal('hide');
+      $('#signup-modal-error').modal();
+      $scope.newUser.errorMessage.push(
+          'Please verify the information you have entered.');
+      return;
+    }
+
+    // Do some validation here.
+    var postUser = {};
+    postUser.email = $scope.newUser.email;
+    postUser.firstName = $scope.newUser.firstName;
+    postUser.lastName = $scope.newUser.lastName;
+    postUser.password = $scope.newUser.password;
+
+    $http.post('/api/users', {
+      email: postUser.email,
+      firstName: postUser.firstName,
+      lastName: postUser.lastName,
+      password: postUser.password,
+      role: ($scope.newUser.isProfessor) ? 'Professor' : 'Student'
+    }).success(function() {
+      $('#signup-modal').modal('hide');
+      $('#signup-modal-success').modal();
+
+      // Immediately logging in the user here does not work, because the modal
+      // does not finish hiding before angular changes the browser's location.
+      refreshUsers();
+    }).error(function(res, data) {
+      $scope.newUser.errorMessage = [];
+      if (res.error && res.error.toLowerCase() === 'email already taken') {
+        $scope.newUser.errorMessage.push('E-mail address already taken.\n');
+      } else {
+        res.forEach(function(entry) {
+          if (entry.property === 'firstName') {
+            $scope.newUser.errorMessage.push(
+                'First Name ' + entry.message + '\n');
+          }
+
+          if (entry.property === 'lastName') {
+            $scope.newUser.errorMessage.push(
+                'Last Name ' + entry.message + '\n');
+          }
+
+          if (entry.property === 'email') {
+            $scope.newUser.errorMessage.push(
+                'The email address you entered is invalid.');
+          }
+
+        });
+      }
+      
+      $('#signup-modal').modal('hide');
+      $('#signup-modal-error').modal();
+    });
+  };
+
 }]);
